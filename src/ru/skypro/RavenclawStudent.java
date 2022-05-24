be6295b3cfed365cc15dfa8f3c6a6ce9fbc4614e
NIKO <nzubovskiy@gmail.com>
2022-05-24 18:59:20 +0400

    Домашнее задание наконец-таки

---
+++ src/ru/skypro/RavenclawStudent.java
@@ -0,0 +1,87 @@
+package ru.skypro;
+
+import java.util.Objects;
+
+public class RavenclawStudent extends HogwartsStudent {
+    private int intelligence;
+    private int wisdom;
+    private int wit;
+    private int creativity;
+
+    public RavenclawStudent(String name, int magicPower, int transgressionDistance, int intelligence, int wisdom, int wit, int creativity) {
+        super(name, magicPower, transgressionDistance);
+        this.intelligence = intelligence;
+        this.wisdom = wisdom;
+        this.wit = wit;
+        this.creativity = creativity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getWit() {
+        return wit;
+    }
+
+    public void setWit(int wit) {
+        this.wit = wit;
+    }
+
+    public int getCreativity() {
+        return creativity;
+    }
+
+    public void setCreativity(int creativity) {
+        this.creativity = creativity;
+    }
+
+    @Override
+    public int ability() {
+        return intelligence + wisdom + wit + creativity;
+    }
+
+    public void compareRavenclaw(RavenclawStudent ravenclawStudent) {
+        int ability1 = ability();
+        int ability2 = ravenclawStudent.ability();
+        if (ability1 > ability2) {
+            System.out.println(getName() + " лучший Когтевранец, чем " + ravenclawStudent.getName());
+        } else if (ability2 > ability1) {
+            System.out.println(ravenclawStudent.getName() + " лучший Когтевранец, чем " + getName());
+        } else {
+            System.out.println(ravenclawStudent.getName() + " такой же Когтевранец, как " + getName());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RavenclawStudent that = (RavenclawStudent) o;
+        return intelligence == that.intelligence && wisdom == that.wisdom && wit == that.wit && creativity == that.creativity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), intelligence, wisdom, wit, creativity);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", ум: " + intelligence + ", мудрость: " + wisdom + ", остроумность: " + wit + ", креативность: " + creativity;
+    }
+}
+
---
+++ src/ru/skypro/SlytherinStudent.java
@@ -0,0 +1,96 @@
+package ru.skypro;
+
+import java.util.Objects;
+
+public class SlytherinStudent extends HogwartsStudent {
+    private int cunning;
+    private int determination;
+    private int ambition;
+    private int resourcefulness;
+    private int lustPower;
+
+    public SlytherinStudent(String name, int magicPower, int transgressionDistance, int cunning, int determination, int ambition, int resourcefulness, int lustPower) {
+        super(name, magicPower, transgressionDistance);
+        this.cunning = cunning;
+        this.determination = determination;
+        this.ambition = ambition;
+        this.resourcefulness = resourcefulness;
+        this.lustPower = lustPower;
+    }
+
+    public int getCunning() {
+        return cunning;
+    }
+
+    public void setCunning(int cunning) {
+        this.cunning = cunning;
+    }
+
+    public int getDetermination() {
+        return determination;
+    }
+
+    public void setDetermination(int determination) {
+        this.determination = determination;
+    }
+
+    public int getAmbition() {
+        return ambition;
+    }
+
+    public void setAmbition(int ambition) {
+        this.ambition = ambition;
+    }
+
+    public int getResourcefulness() {
+        return resourcefulness;
+    }
+
+    public void setResourcefulness(int resourcefulness) {
+        this.resourcefulness = resourcefulness;
+    }
+
+    public int getLustPower() {
+        return lustPower;
+    }
+
+    public void setLustPower(int lustPower) {
+        this.lustPower = lustPower;
+    }
+
+    @Override
+    public int ability() {
+        return cunning + determination + ambition + resourcefulness + lustPower;
+    }
+
+    public void compareSlytherin(SlytherinStudent slytherinStudent) {
+        int ability1 = ability();
+        int ability2 = slytherinStudent.ability();
+        if (ability1 > ability2) {
+            System.out.println(getName() + " лучший Слизеринец, чем " + slytherinStudent.getName());
+        } else if (ability2 > ability1) {
+            System.out.println(slytherinStudent.getName() + " лучший Слизеринец, чем " + getName());
+        } else {
+            System.out.println(slytherinStudent.getName() + " такой же Слизеринец, как " + getName());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        SlytherinStudent that = (SlytherinStudent) o;
+        return cunning == that.cunning && determination == that.determination && ambition == that.ambition && resourcefulness == that.resourcefulness && lustPower == that.lustPower;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), cunning, determination, ambition, resourcefulness, lustPower);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", хитрость: " + cunning + ", решительность: " + determination + ", амбициозность: " + ambition + ", находчивость: " + resourcefulness + ", жажда власти: " + lustPower;
+    }
+}
